545697c41030eb927864f9ff270cffe72c2a287a
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2022-10-28 16:54:57 +0200

    improves action

--- mvn.sh
+++ mvn.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export MAVEN_IMAGE=brunoe/maven:3.8.6-eclipse-temurin-17
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 docker run \
           --env GITHUBLOGIN=$GITHUBLOGIN \
           --env GITHUBPASSWORD=$GITHUBPASSWORD \
@@ -12,4 +13,4 @@ docker run \
           --env PUID=`id -u` -e PGID=`id -g` \
           --env MAVEN_CONFIG=/var/maven/.m2 \
           $MAVEN_IMAGE \
-          runuser --user user --group user -- mvn -B -e -T 1C -Duser.home=/var/maven --settings /usr/src/mymaven/CI_Java/ci-settings.xml "$@"
+          runuser --user user --group user -- mvn -B -e -T 1C -Duser.home=/var/maven --settings ${SCRIPT_DIR}/ci-settings.xml "$@"
